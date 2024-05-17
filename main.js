5019be64fc6036c3b31fdd54f91f2f018acdc601
toobasaleem123 <166385442+toobasaleem123@users.noreply.github.com>
2024-05-17 01:21:25 -0700

    adding word count project

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log("=".repeat(60));
+console.log(chalk.bold.cyanBright("\t\t WELCOME-code-with-tooba-words-count-\t"));
+console.log("=".repeat(60));
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your words to count:"
+    }
+]);
+let words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your words count are:${words.length}`);
+console.log(chalk.red.bold("\n\t THANK-YOU FOR USING!\n\t"));
